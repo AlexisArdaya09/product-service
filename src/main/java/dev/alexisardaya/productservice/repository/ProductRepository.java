f7bf2139adae5801fb584863f1e711ed4e1c111a
Alexis Ardaya <aardaya@pollux.finance>
2025-10-24 20:43:07 -0400

    feat: Implementacion de un CRUD + JPA + Postgres

---
+++ src/main/java/dev/alexisardaya/productservice/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package dev.alexisardaya.productservice.repository;
+
+import dev.alexisardaya.productservice.model.Product;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  List<Product> findByNameContainingIgnoreCase(String name);
+}
